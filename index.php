b17084661624dd6faafb92ec50138f94aa862f83
MaxvanSchoonderwoerd <maxvanschoonderwoerd@gmail.com>
2022-03-30 11:07:06 +0200

    added header

--- index.php
+++ index.php
@@ -4,9 +4,15 @@ include 'template.php';
 ?>
 
 <?= template_head("Home") ?>
+
 <body>
 
 <header>
+    <div class="header">
+        <a href="#" class="logo"><img src="source/(NEW)%20Wasted%20logo.png" alt=""></a>
+        <a href="#" class="favBtn"><i class="fa-solid fa-heart"></i></a>
+        <a href="#" class="hamburgerBtn"><i class="fa-solid fa-bars"></i></a>
+    </div>
 
 </header>
 
