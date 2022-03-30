080b761f2e63bfe30dfbbafb1fce7cf864bd3af6
QuintenvDriel <quintdriel@msn.com>
2022-03-30 11:12:07 +0200

    icon script

--- template.php
+++ template.php
@@ -10,7 +10,7 @@ function template_head($page)
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../css/style.css">
-    <script src="https://kit.fontawesome.com/5b818eb41b.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/43cf26c0ce.js" crossorigin="anonymous"></script>
     <title>Wasted | $page</title>
 </head>
 EOT;
