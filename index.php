7b8f07f418beb5fdd185329365cc1d98802afa5f
QuintenvDriel <quintdriel@msn.com>
2022-03-30 11:38:31 +0200

    icon script

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include 'template.php';
 
 <header>
     <div class="header">
-        <a href="#" class="logo"><img src="source/(NEW)%20Wasted%20logo.png" alt=""></a>
+        <a href="#" class="logo"><img src="source/(NEW) Wasted logo-geen tekst.png" alt=""></a>
         <a href="#" class="favBtn"><i class="fa-solid fa-heart"></i></a>
         <a href="#" class="hamburgerBtn"><i class="fa-solid fa-bars"></i></a>
     </div>
